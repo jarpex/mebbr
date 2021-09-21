c5a8ecc7c827c5ef2058bad4d7e907a0181f408e
jarpex <default@jarpex.ru>
2021-09-21 22:06:49 +0300

    Refacrtoring in progress

--- bckp/single.php
+++ bckp/single.php
@@ -1335,5 +1335,4 @@ contains:[g,i,e.C_BLOCK_COMMENT_MODE]
 },e.C_LINE_COMMENT_MODE,e.C_BLOCK_COMMENT_MODE]},b]}}})());
 </script>
 <script async type="text/javascript">hljs.initHighlighting();</script>
-<script async type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(43402149, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/43402149" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 </html>
