312b8f827aec7e722efd5dbaeb16335ca32b895a
jarpex <default@jarpex.ru>
2022-10-19 16:36:17 +0300

    Page template

--- page.php
+++ page.php
@@ -1,13 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying all single posts
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package MEBBR
  */
@@ -21,7 +16,48 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', get_post_type() );
+
+            $categories = get_the_category($post->ID);
+            if ($categories) {
+                echo '<div id="related">';
+                $category_ids = array();
+                foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+                $args=array(
+                    'category__in' => $category_ids,
+                    'post__not_in' => array($post->ID),
+                    'showposts'=>5,
+                    'orderby'=>rand(),
+                    'caller_get_posts'=>1);
+                $my_query = new wp_query($args);
+                if( $my_query->have_posts() ) {
+                    echo '<div id="related-header"><h2>Материалы по теме</h2><img draggable="false" role="img" height="30" width="30" class="emoji" alt="🔮" src="//static.mebbr.ru/fonts/mebbr/1f52e.svg"></div><div id="related-container"><ul>';
+                    while ($my_query->have_posts()) {
+                        $my_query->the_post();
+                          if( ! empty( $post->post_title ) ) {
+							echo '<li><a class="rel-title" href="' . get_permalink() . '">';
+							the_title();
+							echo '</a></li>';
+						  }
+                    }
+                }
+                wp_reset_query();
+                echo '</ul></div><hr>';
+			}
+        	if( has_tag() ) {
+				echo '<div id="article_tags">';
+            
+                $posttags = get_the_tags();
+                $html = "";
+                if( $posttags ){
+                    foreach( $posttags as $tag ){
+                        $html .= '<a href="' . esc_attr( get_tag_link( $tag->term_id ) ) . '">' . $tag->name . '</a>';
+                    }
+                    echo $html;
+                }
+            	echo '</div>';
+			}
+        	echo '</div>';
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -30,7 +66,6 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
-
 	</main><!-- #main -->
 
 <?php
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying page content in page.php
+ * Template part for displaying posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -9,16 +9,52 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="main" class="hyphenate">
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div id="autor_card">
+				<a href="<?php echo get_the_author_meta('url'); ?>" target="_blank"><?php echo get_avatar( get_the_author_meta('user_email'), 50 ); ?></a>
+				<div class="post_meta">
+					<a id="autor_url" href="<?php echo get_the_author_meta('url'); ?>" target="_blank"><?php the_author(); ?></a>
+					<span class="post_date"><?php
+                        $pdate = get_the_date("Y", $echo = false);
+                        $cdate = date("Y");
+                        if ($pdate == $cdate){
+                            echo get_the_date("d F", $echo = false);
+                        }else{
+                            the_date();
+                        }
+                    ?></span>
+					<span class="reading_time"><?php _e( '🕑', ' ' ); ?> <?php echo gp_read_time(); ?> <?php _e( 'мин', ' ' ); ?></span>
+				</div>
+			</div><!-- .entry-meta -->
+		<?php endif; 
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+		?>
 	</header><!-- .entry-header -->
-
-	<?php mebbr_post_thumbnail(); ?>
-
+	
 	<div class="entry-content">
 		<?php
-		the_content();
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'mebbr' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				wp_kses_post( get_the_title() )
+			)
+		);
 
 		wp_link_pages(
 			array(
@@ -28,27 +64,4 @@
 		);
 		?>
 	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'mebbr' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
