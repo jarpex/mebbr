a6a46cd390a04143c83c5fe7571d9d17893e1b3d
jarpex <default@jarpex.ru>
2022-10-27 19:37:04 +0300

    Category template basic

--- template-parts/content-index.php
+++ template-parts/content-index.php
@@ -14,37 +14,42 @@
 }
 ?>
 
-<article id="main" class="hyphenate">
-	
-	<div class="entry-content">
+<div class="card">
+	<a href="<?php the_permalink(); ?>">
+		<img class="card_img" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" onerror="this.style.display = 'none'" />
+	</a>
+	<div class="padding">
 		<?php
-        if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-        
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'mebbr' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mebbr' ),
-				'after'  => '</div>',
-			)
-		);
+			if ( is_singular() ) :
+				the_title( '<h1 class="title">', '</h1>' );
+			else :
+				the_title( '<h2 class="title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
 		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-<?php the_ID(); ?> -->
+		<div class="cat">
+			<?php the_category(); ?>
+		</div>
+		<?php the_content(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Читать<span class="screen-reader-text"> "%s"</span>', 'mebbr' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
+
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mebbr' ),
+					'after'  => '</div>',
+				)
+			);
+		?>
+	</div>
+</div><!-- .entry-content -->
